0ae31ea3fca92600b04a4e01a3df7ecb6a2b8d1c
ifobos <juancarlos.garcia.alfaro@gmail.com>
2015-06-17 20:10:04 -0600

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Juan Garcia on 17/6/15.
+//  Copyright (c) 2015 Juan Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by Juan Garcia on 17/6/15.
+//  Copyright (c) 2015 Juan Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
